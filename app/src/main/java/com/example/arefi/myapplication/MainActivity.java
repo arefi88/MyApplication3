c50b1b680cc066ac667c4f086748a9b10de7576a
arefi88 <arefi.mohammad88@gmail.com>
2019-01-06 05:16:31 -0800

    first commit

---
+++ app/src/main/java/com/example/arefi/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.arefi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"hi world!",Toast.LENGTH_LONG).show();
+    }
+}
