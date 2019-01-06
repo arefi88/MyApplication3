79af7e5abe7e0b438ddf329d1338409af58d6c9a
arefi88 <arefi.mohammad88@gmail.com>
2019-01-06 05:56:57 -0800

    first commit

--- app/src/main/java/com/example/arefi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/arefi/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"hi world!",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"salam world!",Toast.LENGTH_LONG).show();
     }
 }
